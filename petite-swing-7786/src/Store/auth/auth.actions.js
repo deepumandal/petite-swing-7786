6ad032d0b949c077e49a9c814462b2f0cc322b82
VishalBhuse <vbhuse1997@gmail.com>
2022-08-25 09:15:22 +0530

    fw17_0623_day3

---
+++ petite-swing-7786/src/Store/auth/auth.actions.js
@@ -0,0 +1,35 @@
+// Auth Actions here
+import axios from "axios";
+import {
+  AUTH_SIGN_IN_ERROR,
+  AUTH_SIGN_IN_LOADING,
+  AUTH_SIGN_IN_SUCCESS,
+  AUTH_SIGN_OUT,
+} from "./auth.types";
+
+export const loginAPI = (creds) => async (dispatch) => {
+  dispatch({ type: AUTH_SIGN_IN_LOADING });
+  try {
+    const res = await axios.post("https://reqres.in/api/login", creds);
+    dispatch({
+      type: AUTH_SIGN_IN_SUCCESS,
+      payload: {
+        token: res.data.token,
+        isAuthenticated: true,
+      },
+    });
+    return res.data;
+  } catch (e) {
+    dispatch({ type: AUTH_SIGN_IN_ERROR });
+
+    console.log(e.message);
+  }
+};
+
+export const logout = () => ({
+  type: AUTH_SIGN_OUT,
+  payload: {
+    token: null,
+    isAuthenticated: false,
+  },
+});
